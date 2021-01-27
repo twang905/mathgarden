8eb598446d724a07403eda3f50aa0b564bac1d3e
twang905 <77583346+twang905@users.noreply.github.com>
2021-01-27 15:02:33 -0500

    Add files via upload

---
+++ maths.js
@@ -0,0 +1,51 @@
+
+var answer;
+var score = 0;
+var backgroundImages = [];
+
+function nextQuestion() 
+{
+    const n1 = Math.floor(Math.random() * 5);
+    document.getElementById('n1').innerHTML = n1;
+
+    const n2 = Math.floor(Math.random() * 6);
+    document.getElementById('n2').innerHTML = n2;
+
+    answer = n1 + n2;
+}
+
+function checkAnswer() {
+    
+    const prediction = predictImage();
+    
+    if (prediction == answer)
+    {
+        score++;
+        if (score <= 6)
+        {
+            backgroundImages.push(`url('images/background${score}.svg')`);
+            document.body.style.backgroundImage = backgroundImages;
+        }
+        else
+        {
+            alert('Well done your math garden is in full bloom! Want to start again?');
+            score = 0;
+            backgroundImages = [];
+            document.body.style.backgroundImage = backgroundImages;
+        }
+    }
+    else
+    {
+        if (score != 0) {score--;}
+        alert('Oops! Check your calculations and try writing the number neater next time!');
+        setTimeout(
+            function () 
+            {
+                backgroundImages.pop();
+                document.body.style.backgroundImage = backgroundImages;
+            }, 
+            1000
+        );
+    }
+}
+
